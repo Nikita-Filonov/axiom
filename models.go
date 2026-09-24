d83f612b1e2907fb39cc2a8741d60c8d7f491e70
Nikita Filonov <nikita.filonov@dif.tech>
2026-09-24 17:47:32 +0400

    doc: models

--- models.go
+++ models.go
@@ -1,13 +1,16 @@
 package axiom
 
+// Normalize fills defaults or initializes an object's internal state.
 type Normalize interface {
 	Normalize()
 }
 
+// Join describes values that can merge with another value of the same type.
 type Join[T any] interface {
 	Join(other T) T
 }
 
+// Copy describes values that can return an independent configuration copy.
 type Copy[T any] interface {
 	Copy() T
 }
